69646fd94d334ed56218142a0654784784af2175
Morgan Quirk <morganquirk@gmail.com>
2015-10-24 19:18:37 -0700

    udp tests

---
+++ test/udpclient.py
@@ -0,0 +1,11 @@
+import socket
+import time
+
+addr = "192.168.1.17"
+port = 1112
+
+sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+
+for i in range(1100):
+	sock.sendto(str(i), (addr, port))
+	#time.sleep(0.01)
